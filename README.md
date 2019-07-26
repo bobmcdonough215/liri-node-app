96f3c0573b65d9504c426843feffd9691f6034f8
Bob McDonough <bobmcdonough215@gmail.com>
2019-07-26 10:55:53 -0400

    added ReadMe

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# liri-node-app
+#Purpose
+The purpose of this project was to make a Node.js app that depends on user input from the command line. The user would open Ternminal and input node, file name, the command, and a random movie, artist, song, or "do-what-it-says" in the .txt file. This would use API calls and parse through returned JSON objects, outputting them in a specified format.
+
+
+#Technologies Used
+Node.js
+JavaScript
+Bands In Town API 
+Spotify API 
+OMDb API 
--- liri.js
+++ liri.js
@@ -1,107 +1,147 @@
+
 require("dotenv").config();
 
 var keys = require("./keys.js");
 var Spotify = require("node-spotify-api");
 var spotify = new Spotify(keys.spotify);
-var fs =require("fs");
+var fs = require("fs");
 var axios = require("axios");
 
 var command = process.argv[2];
 console.log(command);
 
 
-  
-  var searchTerm = process.argv.slice(3).join(" ");
-  console.log(searchTerm);
+
+var searchTerm = process.argv.slice(3).join(" ");
+console.log(searchTerm);
 
 
 switch (command) {
-    case "concert-this":
-concert(searchTerm);
-        break;
-    case "spotify-this-song":
-        spotifyThis(searchTerm);
-        break;
-    case "movie-this":
-        searchOMDB(searchTerm);
-        break;
-    case "do-what-it-says":
-        doWhatItSays();
-        break;
-    default:
-        console.log("Please enter a valid search");
+  case "concert-this":
+    concert(searchTerm);
+    break;
+  case "spotify-this-song":
+    spotifyThis(searchTerm);
+    break;
+  case "movie-this":
+    searchOMDB(searchTerm);
+    break;
+  case "do-what-it-says":
+    doWhatItSays();
+    break;
+  default:
+    console.log("Please enter a valid search");
 }
 
-function concert(){
-console.log("");
+function concert(artist) {
+  console.log("this is the concert");
+  var queryUrl = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp";
 
+  // This line is just to help us debug against the actual URL.
+  console.log(queryUrl);
 
-//almost the same as OMDB
-//use axios to call
-//var queryUrl = 
-// Call bands in town API
-
-//https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp
-
-// step 2 get the response and console log the necessaery data, data is found in instructions
+  axios.get(queryUrl).then(
+    function (response) {
+      console.log(response);
+      console.log("Venue " + response.venue.name),
+        console.log("Venue Location " + response.country.region.city),
+        console.log("Date of event " + response.datetime)
+    })
+    .catch(function (error) {
+      if (error.response) {
+        // The request was made and the server responded with a status code
+        // that falls out of the range of 2xx
+        console.log("---------------Data---------------");
+        console.log(error.response.data);
+        console.log("---------------Status---------------");
+        console.log(error.response.status);
+        console.log("---------------Status---------------");
+        console.log(error.response.headers);
+      } else if (error.request) {
+        // The request was made but no response was received
+        // `error.request` is an object that comes back with details pertaining to the error that occurred.
+        console.log(error.request);
+      } else {
+        // Something happened in setting up the request that triggered an Error
+        console.log("Error", error.message);
+      }
+      console.log(error.config);
+    })
 
 
 }
-function spotifyThis (song){
-    spotify.search({ type: 'track', query: song }, function(err, data) {
-        if (err) {
-          return console.log('Error occurred: ' + err);
-        }
-       
-      console.log(data.tracks.items[0].artists[0.external_urls]); 
-      });
-console.log("this is spotify");
+function spotifyThis(song) {
+  spotify.search({ type: 'track', query: song }, function (err, data) {
+    if (err) {
+      return console.log('Error occurred: ' + err);
+    }
 
-// call the spotify API
+    console.log(data.tracks.items[0].artists[0].external_urls);
+  });
+  console.log("this is spotify");
 
-//  step 2 get the response and console log the necessaery data, data is found in instructions
+  // call the spotify API
+
+  //  step 2 get the response and console log the necessaery data, data is found in instructions
 }
 
 //Search OMDB Function
-function searchOMDB (movie){
-    console.log("this is the movie");
-    var queryUrl = "http://www.omdbapi.com/?t=" + movie + "&y=&plot=short&apikey=trilogy";
+function searchOMDB(movie) {
+  console.log("this is the movie");
+  var queryUrl = "http://www.omdbapi.com/?t=" + movie + "&y=&plot=short&apikey=trilogy";
 
-    // This line is just to help us debug against the actual URL.
-    console.log(queryUrl);
-    
-    axios.get(queryUrl).then(
-      function(response) {
-        console.log(response);
-        console.log("Movie Title: " + response.data.Title)
-        console.log("Movie Title: " + response.data.Title)
-        console.log("Movie Title: " + response.data.Title)
-        console.log("Movie Title: " + response.data.Title)
-      })
-      .catch(function(error) {
-        if (error.response) {
-          // The request was made and the server responded with a status code
-          // that falls out of the range of 2xx
-          console.log("---------------Data---------------");
-          console.log(error.response.data);
-          console.log("---------------Status---------------");
-          console.log(error.response.status);
-          console.log("---------------Status---------------");
-          console.log(error.response.headers);
-        } else if (error.request) {
-          // The request was made but no response was received
-          // `error.request` is an object that comes back with details pertaining to the error that occurred.
-          console.log(error.request);
-        } else {
-          // Something happened in setting up the request that triggered an Error
-          console.log("Error", error.message);
-        }
-        console.log(error.config);
-      });
+  // This line is just to help us debug against the actual URL.
+  console.log(queryUrl);
 
-}
-function doWhatItSays (){
+  axios.get(queryUrl).then(
+    function (response) {
+      console.log(response);
+      console.log("Movie Title: " + response.data.Title),
+        console.log("Release Year: " + response.data.Year),
+        console.log("IMDB Rating: " + response.data.imdbRating),
+        console.log("Rotten Tomatoes Rating: " + response.data.Title),
+        console.log("Country: " + response.data.Country,
+          console.log("Language: " + response.data.Language),
+          console.log("Plot: " + response.data.Plot,
+            console.log("Actors/Actresses: " + response.data.Actors)
+          )
+            .catch(function (error) {
+              if (error.response) {
+                // The request was made and the server responded with a status code
+                // that falls out of the range of 2xx
+                console.log("---------------Data---------------");
+                console.log(error.response.data);
+                console.log("---------------Status---------------");
+                console.log(error.response.status);
+                console.log("---------------Status---------------");
+                console.log(error.response.headers);
+              } else if (error.request) {
+                // The request was made but no response was received
+                // `error.request` is an object that comes back with details pertaining to the error that occurred.
+                console.log(error.request);
+              } else {
+                // Something happened in setting up the request that triggered an Error
+                console.log("Error", error.message);
+              }
+              console.log(error.config);
+            }
+
+            ));
+
+    });
+  function doWhatItSays() {
     console.log("do what it says");
-    // reading the .txt, use fs.readfile
-    // calling the spotify api wiuth that specific song
+    switch (dataArr[0]) {
+      case "concert-this":
+        concert(searchTerm);
+        break;
+      case "spotify-this-song":
+        spotifyThis(searchTerm);
+        break;
+      case "movie-this":
+        searchOMDB(searchTerm);
+        break;
+    }
+  }
+
 }
